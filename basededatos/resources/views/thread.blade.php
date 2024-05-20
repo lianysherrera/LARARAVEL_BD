d3bc9d2179f7209c5956803bbf1de0edece9257b
lily <lianys.herrera@hotmail.com>
2024-05-20 15:00:18 +0200

    Listado de comentarios!

--- basededatos/resources/views/thread.blade.php
+++ basededatos/resources/views/thread.blade.php
@@ -28,4 +28,18 @@
                 {{ $thread->comments->count() }}  Comentarios
             </span>
     </h2>
+    @foreach($thread->comments as $comment)
+    <div class="max-w-3xl">
+        <div class="flex items-center font-bold">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            </svg>
+            {{ $comment->user->name}}
+        </div>
+        <div class="text-sm">
+            {{$comment->body}}
+        </div>
+        <hr class="my-4">
+    </div>
+    @endforeach
 @endsection
